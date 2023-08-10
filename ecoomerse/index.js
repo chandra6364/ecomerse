aa82258f437bce16ac737ed96a858b2223f2e7aa
chandra6364 <141944723+chandra6364@users.noreply.github.com>
2023-08-10 21:22:53 +0530

    Add files via upload

---
+++ ecoomerse/index.js
@@ -0,0 +1,58 @@
+am1=document.getElementById("showMensFashion")
+am1.style.display="none"
+aw1=document.getElementById("showWomensFashion")
+aw1.style.display="none"
+ak1=document.getElementById("showKidsFashion")
+ak1.style.display="none"
+ae1=document.getElementById("showElectronics")
+ae1.style.display="none"
+bd1=document.getElementById("deliverytime")
+bd1.style.display="none"
+bp1=document.getElementById("priceforproducts")
+bp1.style.display="none"
+bc1=document.getElementById("customerreview")
+bc1.style.display="none"
+bpt1=document.getElementById("payingTypes")
+bpt1.style.display="none"
+function mensFashion(){
+    am2=document.getElementById("showMensFashion")
+    am2.style.display=""
+}
+function womensFashion(){
+    aw2=document.getElementById("showWomensFashion")
+    aw2.style.display=""
+}
+function kidsFashion(){
+    ak2=document.getElementById("showKidsFashion")
+    ak2.style.display=""
+}
+function electronics(){
+    ae2=document.getElementById("showElectronics")
+    ae2.style.display=""
+}
+function deliverytime(){
+    bd2=document.getElementById("deliverytime")
+    bd2.style.display=""
+}
+function priceforproducts(){
+    bp2=document.getElementById("priceforproducts")
+    bp2.style.display=""
+}
+function customerreview(){
+    bc2=document.getElementById("customerreview")
+    bc2.style.display=""
+}
+function payingTypes(){
+    bpt2=document.getElementById("payingTypes")
+    bpt2.style.display=""
+}
+function mouseleave(){
+    am1=document.getElementById("showMensFashion")
+am1.style.display="none"
+aw1=document.getElementById("showWomensFashion")
+aw1.style.display="none"
+ak1=document.getElementById("showKidsFashion")
+ak1.style.display="none"
+ae1=document.getElementById("showElectronics")
+ae1.style.display="none"
+}
